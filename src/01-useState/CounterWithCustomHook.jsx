dbbdb5c2eaa37435316d3b53a65190d8bdbd226f
johanicp123 <johanicp123@gmail.com>
2023-01-23 03:08:48 -0500

    actuaaizacion

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,14 @@
+import { useCounter } from '../hooks/useCounter';
+
+export const CounterWithCustomHook = () => {
+    const {counter} = useCounter();
+  return (
+    <>
+        <h1>Counter with hook : {counter}</h1>
+        <hr />
+        <button className="btn btn-primary">+1</button>
+        <button className="btn btn-primary">+reset</button>
+        <button className="btn btn-primary">-1</button>
+    </>
+  )
+}
