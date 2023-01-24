8f36180f8e1c3f5c9b7e5871cd7aa96f2f35aceb
johanicp123 <johanicp123@gmail.com>
2023-01-23 20:10:54 -0500

    actualizacion

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -1,14 +1,15 @@
 import { useCounter } from '../hooks/useCounter';
 
 export const CounterWithCustomHook = () => {
-    const {counter} = useCounter();
+    const {counter,increment,decrement,reset} = useCounter();
+   
   return (
     <>
         <h1>Counter with hook : {counter}</h1>
         <hr />
-        <button className="btn btn-primary">+1</button>
-        <button className="btn btn-primary">+reset</button>
-        <button className="btn btn-primary">-1</button>
+        <button className="btn btn-primary" onClick={increment}>+1</button>
+        <button className="btn btn-primary" onClick={reset}>reset</button>
+        <button className="btn btn-primary" onClick={decrement}>-1</button>
     </>
   )
 }
---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username:"alberth",
+        email:"johani@gmail.com"
+    });
+
+    const {username,email} = formState;
+
+    const onInputChange = ({target}) =>{
+        const {name,value}=target;
+        setFormState({
+            ...formState,
+            [name]:value
+        })
+    }
+
+    useEffect(()=>{
+        console.log("useEffect called");
+    },[]);
+
+    useEffect(()=>{
+        console.log("useEffect change");
+    },[formState]);
+
+    useEffect(()=>{
+        console.log("email change");
+    },[email]);
+
+
+  return (
+    <>
+      <h1>Formulario</h1>
+      <hr />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="alberth@gmail.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
