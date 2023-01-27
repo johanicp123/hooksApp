59c86a6494cea5eb0c614fab2bb99e9051b9e84c
johanicp123 <johanicp123@gmail.com>
2023-01-27 17:13:24 -0500

    actualizacion

---
+++ src/06-memos/small.jsx
@@ -0,0 +1,9 @@
+import { memo } from "react";
+
+export const Small = memo(({value}) => {
+console.log("me volvi a generar");
+
+  return (
+    <small>{value}</small>
+  )
+})
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { FocusScreen } from './04-useRef/FocusScreen'
+//import { FocusScreen } from './04-useRef/FocusScreen'
+import { Memorize } from './06-memos/Memorize'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -12,6 +13,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Memorize />
   </React.StrictMode>,
 )
