7eb3e1107ddbc90c492839204c9e3d7b8f5595e0
johanicp123 <johanicp123@gmail.com>
2023-01-27 04:00:36 -0500

    actualizacion

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,23 @@
+import { useFetch } from "../hooks/useFetch";
+
+export const Layout = () => {
+  const { data, isLoading, hasError } = useFetch(
+    "https://pokeapi.co/api/v2/pokemon/1"
+  );
+  const {autor, quote } = !!data && data[0];
+
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer">{autor}</footer>
+        </blockquote>
+      )}
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+//import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -10,6 +12,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <FocusScreen />
   </React.StrictMode>,
 )
